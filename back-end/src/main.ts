edd0f959d62b5a0dfb2d796f8b830660dcd8f785
Edu Guerreiro <eduargolo13@gmail.com>
2026-04-22 21:13:35 -0300

    "commit deploy"

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,15 +5,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: [
+			'http://localhost:5173',
+			'https://SEU-FRONTEND.vercel.app',
+		],
+		credentials: true,
+	});
+
 	const config = new DocumentBuilder()
 		.setTitle('Documentação da api')
 		.setDescription('A descrição da api')
 		.setVersion('1.0')
-		// .addTag('docs')
 		.build();
-	const documentFactory = () => SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('docs', app, documentFactory);
 
-	await app.listen(3000);
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
+	await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
