715b692d6186eca1b684f1231e3e4be4a115f3ce
Sinval Luz <contato.sinvalluz@gmail.com>
2026-04-24 20:26:46 -0300

    feat: configura a conexão com o banco de dados utilizando prisma

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
 	imports: [HealthModule, AuthModule, UserModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, PrismaService],
 })
 export class AppModule {}
